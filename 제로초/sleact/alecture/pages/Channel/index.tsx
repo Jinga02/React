248bd44dbe0248110dafbb36c5cbcb2b4dfbbf44
jinjaehwan <jjh099@icloud.com>
2023-06-28 02:40:35 +0900

    sleact클론코딩 chatbox 500에러 수정중

--- 제로초/sleact/alecture/pages/Channel/index.tsx
+++ 제로초/sleact/alecture/pages/Channel/index.tsx
@@ -1,17 +1,26 @@
 // import Workspace from '@layouts/Workspace';
-import InviteChannelModal from '@components/InviteChannelModal';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+// import InviteChannelModal from '@components/InviteChannelModal';
+import useInput from '@hooks/useInput';
 import { Container, Header } from '@pages/Channel/styles';
 import React, { useCallback, useState } from 'react';
 // Workspace로 감싸면 안의 div태그가 Workspace.tsx의 children이 됨
 // 다른 JSX안에 넣으면 그 JSX의 children이 된다.
 const Channel = () => {
-  const [showInviteChannelModal, setShowInviteChannelModal] = useState(false);
-  const onClickInviteChannel = useCallback(() => {
-    setShowInviteChannelModal(true);
-  }, []);
+  // const [showInviteChannelModal, setShowInviteChannelModal] = useState(false);
+  // const onClickInviteChannel = useCallback(() => {
+  //   setShowInviteChannelModal(true);
+  // }, []);
 
-  const onCloseModal = useCallback(() => {
-    setShowInviteChannelModal(false);
+  // const onCloseModal = useCallback(() => {
+  //   setShowInviteChannelModal(false);
+  // }, []);
+
+  const [chat, onChangeChat, setChat] = useInput('');
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    setChat('');
   }, []);
   return (
     // <Workspace>
@@ -19,9 +28,10 @@ const Channel = () => {
     // </Workspace>
     <Container>
       <Header>
-        {/* <span>#{channel}</span> */}
+        채널
+        {/* <span>#{channel}</span>
         <div style={{ display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
-          {/* <span>{channelMembersData?.length}</span> */}
+          <span>{channelMembersData?.length}</span>
           <button
             onClick={onClickInviteChannel}
             className="c-button-unstyled p-ia__view_header__button"
@@ -31,13 +41,27 @@ const Channel = () => {
           >
             <i className="c-icon p-ia__view_header__button_icon c-icon--add-user" aria-hidden="true" />
           </button>
-        </div>
+        </div> */}
       </Header>
-      <InviteChannelModal
+      <ChatList
+      // scrollbarRef={scrollbarRef}
+      // isReachingEnd={isReachingEnd}
+      // isEmpty={isEmpty}
+      // chatSections={chatSections}
+      // setSize={setSize}
+      />
+      <ChatBox
+        onSubmitForm={onSubmitForm}
+        chat={chat}
+        onChangeChat={onChangeChat}
+        // placeholder={`Message #${channel}`}
+        // data={channelMembersData}
+      />
+      {/* <InviteChannelModal
         show={showInviteChannelModal}
         onCloseModal={onCloseModal}
         setShowInviteChannelModal={setShowInviteChannelModal}
-      />
+      /> */}
     </Container>
   );
 };
--- 제로초/sleact/alecture/pages/DirectMessage/index.tsx
+++ 제로초/sleact/alecture/pages/DirectMessage/index.tsx
@@ -1,33 +1,58 @@
 import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
-import useSWR from 'swr';
+import useSWR, { useSWRInfinite } from 'swr';
 import fetcher from '@utils/fetcher';
 import { useParams } from 'react-router';
 import gravatar from 'gravatar';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR('/api/users', fetcher);
-  const [chat, onChangeChat] = useInput('');
-  const onSubmitForm = useCallback(() => {}, []);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    revalidate,
+  } = useSWR<IDM>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+            setChat('');
+            console.log('submit');
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) {
     return null;
   }
-  console.log(userData);
 
   return (
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
-
         <span>{userData.nickname}</span>
       </Header>
       <ChatList />
-      <ChatBox chat="" />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
