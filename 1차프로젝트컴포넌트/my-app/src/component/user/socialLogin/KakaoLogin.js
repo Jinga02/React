3ddf422e5f323f10d8dd2658c45d01d1487f33bd
Jinga02 <jjh099@icloud.com>
2023-07-19 17:57:20 +0900

    소셜로긍;니인

--- 1차프로젝트컴포넌트/my-app/src/component/user/socialLogin/KakaoLogin.js
+++ 1차프로젝트컴포넌트/my-app/src/component/user/socialLogin/KakaoLogin.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const KakaoLogin = () => {
-  const CLIENT_ID = "057b020ef203b7f5c7858d810b217069";
   const REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
